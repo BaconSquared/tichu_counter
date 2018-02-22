1152a7460e98842648348775784112363e21e126
Matt Klocek <nwbm87@motorola.com>
2018-02-22 13:46:47 -0600

    Create initial layout of home and score screens

---
+++ main.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+# from kivy.uix.widget import Widget
+
+class MenuScreen(Screen):
+    pass
+
+class HomeScreen(Screen):
+    pass
+
+class ScoreScreen(Screen):
+    pass
+
+class MyApp(App):
+    pass
+
+if __name__ == "__main__":
+    MyApp().run()
